b06b37aa132b276277fa4479cf05d91206f37181
reciokaue <kaue.recio2@gmail.com>
2024-09-17 12:29:13 -0300

    not found

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,13 +1,16 @@
 <script setup>
-  import { CircleAlert } from 'lucide-vue-next';
+  import { TriangleAlert } from 'lucide-vue-next';
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center h-screen space-y-6 pb-40">
-    <h1 class="text-3xl font-bold text-center">
-      Page Not Found
-    </h1>
-    <CircleAlert class="size-20 text-gray-400" />
-    <RouterLink to="/" class="text-white bg-green-700 hover:bg-green-800 px-4 py-2 rounded-md">Go to Home</RouterLink>
-  </div>
+  <section class="text-center flex flex-col justify-center items-center h-96 mt-20">
+    <TriangleAlert class="size-40 text-yellow-400  mb-4"/>
+    <h1 class="text-6xl font-bold mb-4">404 Not Found</h1>
+    <p class="text-xl mb-5">This page does not exist</p>
+    <a
+      href="index.html"
+      class="text-white bg-green-700 hover:bg-green-900 rounded-md px-3 py-2 mt-4"
+      >Go Back</a
+    >
+  </section>
 </template>
